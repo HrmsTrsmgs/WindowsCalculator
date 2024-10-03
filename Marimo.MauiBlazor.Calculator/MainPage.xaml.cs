c7d032f57636fd7d296912cd03db21c112a340d8
NITTA Mitsuhiro <hrmstrsmgs@gmail.com>
2024-10-04 03:59:55 +0900

    初期設定完了。HelloWorldとプロジェクト後世まで。

---
+++ Marimo.MauiBlazor.Calculator/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Marimo.MauiBlazor.Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

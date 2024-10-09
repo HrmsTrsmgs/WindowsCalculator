42ba2446c83bf9fbe086920eec0340c4ed7c46c6
NITTA Mitsuhiro <hrmstrsmgs@gmail.com>
2024-10-10 03:01:38 +0900

    キー表示のプロパティのモデルを作成

---
+++ Models/PropertySettings.cs
@@ -0,0 +1,260 @@
+﻿namespace Marimo.MauiBlazor.Models;
+
+/// <summary>
+/// テンキーに表示する文字を管理します。
+/// </summary>
+public class PropertySettings : ModelBase
+{
+    /// <summary>
+    /// 1のキーのテキストです。
+    /// </summary>
+    string oneDisplayText = "1";
+
+    /// <summary>
+    /// 1のキーのテキストを取得、設定します。
+    /// </summary>
+    public string OneDisplayText
+    {
+        get => oneDisplayText;
+        set => SetProperty(ref oneDisplayText, value);
+    }
+
+    /// <summary>
+    /// 2のキーのテキストです。
+    /// </summary>
+    string twoDisplayText = "2";
+
+    /// <summary>
+    /// 2のキーのテキストを取得、設定します。
+    /// </summary>
+    public string TwoDisplayText
+    {
+        get => twoDisplayText;
+        set => SetProperty(ref twoDisplayText, value);
+    }
+
+    /// <summary>
+    /// 3のキーのテキストです。
+    /// </summary>
+    string threeDisplayText = "3";
+
+    /// <summary>
+    /// 3のキーのテキストを取得、設定します。
+    /// </summary>
+    public string ThreeDisplayText
+    {
+        get => threeDisplayText;
+        set => SetProperty(ref threeDisplayText, value);
+    }
+
+    /// <summary>
+    /// 4のキーのテキストです。
+    /// </summary>
+    string fourDisplayText = "4";
+
+    /// <summary>
+    /// 4のキーのテキストを取得、設定します。
+    /// </summary>
+    public string FourDisplayText
+    {
+        get => fourDisplayText;
+        set => SetProperty(ref fourDisplayText, value);
+    }
+    /// <summary>
+    /// 5のキーのテキストです。
+    /// </summary>
+    string fiveDisplayText = "5";
+
+    /// <summary>
+    /// 5のキーのテキストを取得、設定します。
+    /// </summary>
+    public string FiveDisplayText
+    {
+        get => fiveDisplayText;
+        set => SetProperty(ref fiveDisplayText, value);
+    }
+
+    /// <summary>
+    /// 6のキーのテキストです。
+    /// </summary>
+    string sixDisplayText = "6";
+
+    /// <summary>
+    /// 6のキーのテキストを取得、設定します。
+    /// </summary>
+    public string SixDisplayText
+    {
+        get => sixDisplayText;
+        set => SetProperty(ref sixDisplayText, value);
+    }
+
+    /// <summary>
+    /// 7のキーのテキストです。
+    /// </summary>
+    string sevenDisplayText = "7";
+
+    /// <summary>
+    /// 7のキーのテキストを取得、設定します。
+    /// </summary>
+    public string SevenDisplayText
+    {
+        get => sevenDisplayText;
+        set => SetProperty(ref sevenDisplayText, value);
+    }
+    /// <summary>
+    /// 8のキーのテキストです。
+    /// </summary>
+    string eightDisplayText = "8";
+
+    /// <summary>
+    /// 8のキーのテキストを取得、設定します。
+    /// </summary>
+    public string EightDisplayText
+    {
+        get => eightDisplayText;
+        set => SetProperty(ref eightDisplayText, value);
+    }
+    /// <summary>
+    /// 9のキーのテキストです。
+    /// </summary>
+    string nineDisplayText = "9";
+
+    /// <summary>
+    /// 9のキーのテキストを取得、設定します。
+    /// </summary>
+    public string NineDisplayText
+    {
+        get => nineDisplayText;
+        set => SetProperty(ref nineDisplayText, value);
+    }
+/// <summary>
+/// 0のキーのテキストです。
+/// </summary>
+    string zeroDisplayText = "0";
+
+    /// <summary>
+    /// 0のキーのテキストを取得、設定します。
+    /// </summary>
+    public string ZeroDisplayText
+    {
+        get => zeroDisplayText;
+        set => SetProperty(ref zeroDisplayText, value);
+    }
+    /// <summary>
+    /// .のキーのテキストです。
+    /// </summary>
+    string dotDisplayText = ".";
+
+    /// <summary>
+    /// .のキーのテキストを取得、設定します。
+    /// </summary>
+    public string DotDisplayText
+    {
+        get => dotDisplayText;
+        set => SetProperty(ref dotDisplayText, value);
+    }
+    /// <summary>
+    /// +のキーのテキストです。
+    /// </summary>
+    string plusDisplayText = "+";
+
+    /// <summary>
+    /// +のキーのテキストを取得、設定します。
+    /// </summary>
+    public string PlusDisplayText
+    {
+        get => plusDisplayText;
+        set => SetProperty(ref plusDisplayText, value);
+    }
+    /// <summary>
+    /// -のキーのテキストです。
+    /// </summary>
+    string minusDisplayText = "-";
+
+    /// <summary>
+    /// -のキーのテキストを取得、設定します。
+    /// </summary>
+    public string MinusDisplayText
+    {
+        get => minusDisplayText;
+        set => SetProperty(ref minusDisplayText, value);
+    }
+    /// <summary>
+    /// ×のキーのテキストです。
+    /// </summary>
+    string multiplyDisplayText = "×";
+
+    /// <summary>
+    /// ×のキーのテキストを取得、設定します。
+    /// </summary>
+    public string MultiplyDisplayText
+    {
+        get => multiplyDisplayText;
+        set => SetProperty(ref multiplyDisplayText, value);
+    }
+    /// <summary>
+    /// ÷のキーのテキストです。
+    /// </summary>
+    string divideDisplayText = "÷";
+
+    /// <summary>
+    /// ÷のキーのテキストを取得、設定します。
+    /// </summary>
+    public string DivideDisplayText
+    {
+        get => divideDisplayText;
+        set => SetProperty(ref divideDisplayText, value);
+    }
+    /// <summary>
+    /// ÷のキーのテキストです。
+    /// </summary>
+    string equalDisplayText = "=";
+
+    /// <summary>
+    /// ＝のキーのテキストを取得、設定します。
+    /// </summary>
+    public string EqualDisplayText
+    {
+        get => equalDisplayText;
+        set => SetProperty(ref equalDisplayText, value);
+    }
+    /// <summary>
+    /// Cのキーのテキストです。
+    /// </summary>
+    string cDisplayText = "C";
+
+    /// <summary>
+    /// Cのキーのテキストを取得、設定します。
+    /// </summary>
+    public string CDisplayText
+    {
+        get => cDisplayText;
+        set => SetProperty(ref cDisplayText, value);
+    }
+    /// <summary>
+    /// CEのキーのテキストです。
+    /// </summary>
+    string ceDisplayText = "CE";
+
+    /// <summary>
+    /// ÷のキーのテキストを取得、設定します。
+    /// </summary>
+    public string CEDisplayText
+    {
+        get => ceDisplayText;
+        set => SetProperty(ref ceDisplayText, value);
+    }
+    /// <summary>
+    /// ÷のキーのテキストです。
+    /// </summary>
+    string backspaceDisplayText = "⌫";
+
+    /// <summary>
+    /// ÷のキーのテキストを取得、設定します。
+    /// </summary>
+    public string BackspaceDisplayText
+    {
+        get => backspaceDisplayText;
+        set => SetProperty(ref backspaceDisplayText, value);
+    }
+}
---
+++ ModelsTests/PropertySettingsTests.cs
@@ -0,0 +1,262 @@
+﻿using Marimo.MauiBlazor.Models;
+
+namespace Marimo.MauiBlazor.Tests.Models;
+
+public class PropertySettingsTests
+{
+    PropertySettings tested = new();
+
+    string? changedProperty = null;
+    public PropertySettingsTests()
+    {
+        tested.PropertyChanged += (sender, e) => changedProperty = e.PropertyName;
+    }
+
+    [Fact]
+    public void OneDisplayTextの初期値は1です()
+    {
+        tested.OneDisplayText.Should().Be("1");
+    }
+
+    [Fact]
+    public void OneDisplayTextは変更されたことを通知します()
+    {
+        tested.OneDisplayText = "A";
+        changedProperty.Should().Be("OneDisplayText");
+    }
+
+    [Fact]
+    public void TwoDisplayTextの初期値は2です()
+    {
+        tested.TwoDisplayText.Should().Be("2");
+    }
+
+    [Fact]
+    public void TwoDisplayTextは変更されたことを通知します()
+    {
+        tested.TwoDisplayText = "A";
+        changedProperty.Should().Be("TwoDisplayText");
+    }
+
+    [Fact]
+    public void ThreeDisplayTextの初期値は4です()
+    {
+        tested.ThreeDisplayText.Should().Be("3");
+    }
+
+    [Fact]
+    public void ThreeDisplayTextは変更されたことを通知します()
+    {
+        tested.FiveDisplayText = "A";
+        changedProperty.Should().Be("FiveDisplayText");
+    }
+
+    [Fact]
+    public void FourDisplayTextの初期値は4です()
+    {
+        tested.FourDisplayText.Should().Be("4");
+    }
+
+    [Fact]
+    public void FourDisplayTextは変更されたことを通知します()
+    {
+        tested.FiveDisplayText = "A";
+        changedProperty.Should().Be("FiveDisplayText");
+    }
+
+    [Fact]
+    public void FiveDisplayTextの初期値は5です()
+    {
+        tested.FiveDisplayText.Should().Be("5");
+    }
+
+    [Fact]
+    public void FiveDisplayTextは変更されたことを通知します()
+    {
+        tested.FiveDisplayText = "A";
+        changedProperty.Should().Be("FiveDisplayText");
+    }
+
+    [Fact]
+    public void SixDisplayTextの初期値は6です()
+    {
+        tested.SixDisplayText.Should().Be("6");
+    }
+
+    [Fact]
+    public void SixDisplayTextは変更されたことを通知します()
+    {
+        tested.SixDisplayText = "A";
+        changedProperty.Should().Be("SixDisplayText");
+    }
+
+    [Fact]
+    public void SevenDisplayTextの初期値は7です()
+    {
+        tested.SevenDisplayText.Should().Be("7");
+    }
+
+    [Fact]
+    public void SevenDisplayTextは変更されたことを通知します()
+    {
+        tested.SevenDisplayText = "A";
+        changedProperty.Should().Be("SevenDisplayText");
+    }
+
+    [Fact]
+    public void EightDisplayTextの初期値は8です()
+    {
+        tested.EightDisplayText.Should().Be("8");
+    }
+
+    [Fact]
+    public void EightDisplayTextは変更されたことを通知します()
+    {
+        tested.EightDisplayText = "A";
+        changedProperty.Should().Be("EightDisplayText");
+    }
+
+    [Fact]
+    public void NineDisplayTextの初期値は9です()
+    {
+        tested.NineDisplayText.Should().Be("9");
+    }
+
+    [Fact]
+    public void NineDisplayTextは変更されたことを通知します()
+    {
+        tested.NineDisplayText = "A";
+        changedProperty.Should().Be("NineDisplayText");
+    }
+
+    [Fact]
+    public void ZeroDisplayTextの初期値は0です()
+    {
+        tested.ZeroDisplayText.Should().Be("0");
+    }
+
+    [Fact]
+    public void ZeroDisplayTextは変更されたことを通知します()
+    {
+        tested.ZeroDisplayText = "A";
+        changedProperty.Should().Be("ZeroDisplayText");
+    }
+
+    [Fact]
+    public void DotDisplayTextの初期値は小数点です()
+    {
+        tested.DotDisplayText.Should().Be(".");
+    }
+
+    [Fact]
+    public void DotDisplayTextは変更されたことを通知します()
+    {
+        tested.DotDisplayText = "A";
+        changedProperty.Should().Be("DotDisplayText");
+    }
+
+    [Fact]
+    public void PlusDisplayTextの初期値はプラスです()
+    {
+        tested.PlusDisplayText.Should().Be("+");
+    }
+
+    [Fact]
+    public void PlusDisplayTextは変更されたことを通知します()
+    {
+        tested.PlusDisplayText = "A";
+        changedProperty.Should().Be("PlusDisplayText");
+    }
+
+    [Fact]
+    public void MinusDisplayTextの初期値はマイナスです()
+    {
+        tested.MinusDisplayText.Should().Be("-");
+    }
+
+    [Fact]
+    public void MinusDisplayTextは変更されたことを通知します()
+    {
+        tested.MinusDisplayText = "A";
+        changedProperty.Should().Be("MinusDisplayText");
+    }
+
+    [Fact]
+    public void MultiplyDisplayTextの初期値は掛ける記号です()
+    {
+        tested.MultiplyDisplayText.Should().Be("×");
+    }
+
+    [Fact]
+    public void MultiplyDisplayTextは変更されたことを通知します()
+    {
+        tested.MultiplyDisplayText = "A";
+        changedProperty.Should().Be("MultiplyDisplayText");
+    }
+
+    [Fact]
+    public void DivideDisplayTextの初期値は割る記号です()
+    {
+        tested.DivideDisplayText.Should().Be("÷");
+    }
+
+    [Fact]
+    public void DivideDisplayTextは変更されたことを通知します()
+    {
+        tested.DivideDisplayText = "A";
+        changedProperty.Should().Be("DivideDisplayText");
+    }
+
+    [Fact]
+    public void EqualDisplayTextの初期値はイコール記号です()
+    {
+        tested.EqualDisplayText.Should().Be("=");
+    }
+
+    [Fact]
+    public void EqualDisplayTextは変更されたことを通知します()
+    {
+        tested.EqualDisplayText = "A";
+        changedProperty.Should().Be("EqualDisplayText");
+    }
+
+    [Fact]
+    public void CDisplayTextの初期値はCです()
+    {
+        tested.CDisplayText.Should().Be("C");
+    }
+
+    [Fact]
+    public void CDisplayTextは変更されたことを通知します()
+    {
+        tested.CDisplayText = "A";
+        changedProperty.Should().Be("CDisplayText");
+    }
+
+    [Fact]
+    public void CEDisplayTextの初期値は割る記号です()
+    {
+        tested.CEDisplayText.Should().Be("CE");
+    }
+
+    [Fact]
+    public void CEDisplayTextは変更されたことを通知します()
+    {
+        tested.CEDisplayText = "A";
+        changedProperty.Should().Be("CEDisplayText");
+    }
+
+    [Fact]
+    public void BackspaceDisplayTextの初期値はバックスペース記号です()
+    {
+        tested.BackspaceDisplayText.Should().Be("⌫");
+    }
+
+    [Fact]
+    public void BackspaceDisplayTextは変更されたことを通知します()
+    {
+        tested.BackspaceDisplayText = "A";
+        changedProperty.Should().Be("BackspaceDisplayText");
+    }
+
+}

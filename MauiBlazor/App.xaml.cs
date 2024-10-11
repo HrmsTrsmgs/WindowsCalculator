5451f42330e537c96f5df91a1eaa5720e1c70c68
NITTA Mitsuhiro <hrmstrsmgs@gmail.com>
2024-10-11 11:25:55 +0900

    画面作成一度諦め、Blazor WebAssemblyを始めてみる。

--- MauiBlazor/App.xaml.cs
+++ MauiBlazor/App.xaml.cs
@@ -6,7 +6,26 @@
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new MainPage()
+            {
+                MinimumWidthRequest = 1024
+            };
+        }
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.MinimumWidth = 332;
+            window.MinimumHeight = 504;
+            //window.Width = Preferences.Get("WindowWidth", window.MinimumWidth);
+            //window.Height = Preferences.Get("WindowHeight", window.MinimumHeight);
+            window.Destroying += (s, e) =>
+            {
+                Preferences.Set("WindowWidth", window.Width);
+                Preferences.Set("WindowHeight", window.Height);
+            };
+
+            return window;
         }
     }
 }

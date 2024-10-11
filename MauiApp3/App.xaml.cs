2411e8cfcb750c53d5fdb07d808850825eda288e
NITTA Mitsuhiro <hrmstrsmgs@gmail.com>
2024-10-12 02:15:52 +0900

    作り直しプロジェクトの設定変更完了。

---
+++ MauiApp3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Marimo.WindowsCaliculator.MauiBlazor
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}

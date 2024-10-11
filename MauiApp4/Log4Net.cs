4d9506720b7c3b90329f6d8f0a60eb8db4be4980
NITTA Mitsuhiro <hrmstrsmgs@gmail.com>
2024-10-12 02:50:53 +0900

    Layoutを復旧する。ホームは簡単に動かんようなので後回し。

---
+++ MauiApp4/Log4Net.cs
@@ -0,0 +1,38 @@
+﻿using log4net;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Layout;
+using System.Reflection;
+
+namespace Marimo.WindowsCalculator.Models;
+
+public static class Log4NetExtensions
+{
+    public static IServiceCollection AddLog4Net(this IServiceCollection self, string logFilePath)
+    {
+        var assembly = Assembly.GetEntryAssembly() ?? throw new InvalidOperationException();
+        var repository = LogManager.GetRepository(assembly);
+
+        var layout = new PatternLayout
+        {
+            ConversionPattern = "%date %-5level %logger - %message%n"
+        };
+        layout.ActivateOptions();
+
+#if DEBUG
+        var traceAppender = new TraceAppender { Layout = layout };
+        traceAppender.ActivateOptions();
+        BasicConfigurator.Configure(repository, traceAppender);
+#else
+        var fileAppender = new FileAppender
+        {
+            Layout = layout,
+            File = logFilePath,
+            AppendToFile = true
+        };
+        fileAppender.ActivateOptions();
+        BasicConfigurator.Configure(repository, fileAppender);
+#endif
+        return self;
+    }
+}
